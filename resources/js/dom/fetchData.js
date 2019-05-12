ddcbddf986d572ac549ef074cbf121bffacc9912
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-12 13:59:42 -0700

    Quick view of loading and displaying current ranking data

---
+++ resources/js/dom/fetchData.js
@@ -0,0 +1,15 @@
+var rankings = [];
+
+$.getJSON("resources/data/rankings.json", function(json){
+	rankings = json;
+	console.log(rankings[0]);
+
+	for (var i = rankings.length - 1; i >= 0; i--) {
+		$("tbody").append("<tr><td>" + rankings[i]['fullName'] + 
+			"</td><td>" + rankings[i]['wins'] + 
+			"</td><td>" + rankings[i]['losses']+
+			"</td><td>" + rankings[i]['winRank']+
+			"</td><td>" + rankings[i]['lossRank']+ 
+			"</td></tr>");
+	}
+});
