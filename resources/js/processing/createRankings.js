aafadc3b14839941ab6f5518eed1e1decc4926a8
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-12 13:05:13 -0700

    Project progress so far
    file structure setup, .gitignore definitions

---
+++ resources/js/processing/createRankings.js
@@ -0,0 +1,71 @@
+const fs = require ('fs');
+const _ = require ('underscore');
+
+let rankings = [];
+
+let teams = JSON.parse(fs.readFileSync('teams.json', 'utf8'));
+// console.log(teams);
+
+let standings = JSON.parse(fs.readFileSync('standings.json', 'utf8'));
+// console.log(standings);
+buildSimple(teams,standings);
+sortSimple(rankings);
+
+function buildSimple(teams, standings){
+
+	console.log("buildSimple called");
+
+	//Builds 'ranking' array to be manipulated
+	for (var i = 0; i <= teams.length - 1; i++) {
+		var toBeAdded = {};
+		toBeAdded.fullName = teams[i].fullName;
+		toBeAdded.win = Number(_.findWhere(standings, {teamId: teams[i].teamId}).win);
+		toBeAdded.loss = Number(_.findWhere(standings, {teamId: teams[i].teamId}).loss);
+		rankings.push(toBeAdded);
+	}
+}
+
+function sortSimple(rankings){
+
+	console.log("sortSimple called");
+
+	//Add Win Rank
+	rankings = _.sortBy(rankings, 'win');
+	for (var i = 0; i <= rankings.length - 1; i++) {
+		rankings[i]["winRank"] = rankings.length - i;
+	}
+
+	//Add Loss Rank
+	rankings = _.sortBy(rankings, 'loss');
+	for (var i = 0; i <= rankings.length - 1; i++) {
+		rankings[i]["lossRank"] = rankings.length - i;
+	}
+
+	fs.appendFile("rankings.json", "[", function(err) {if(err) {return console.log(err);} });
+
+	//Print to see results
+	for (var i = rankings.length - 1; i >= 0; i--) {
+		fs.appendFile("rankings.json", "{\"fullName\": \"" + rankings[i]["fullName"] + 
+			"\", \"wins\": " + 
+			rankings[i]["win"] + 
+			", \"losses\": " + 
+			rankings[i]["loss"] +
+			", \"winRank\": " + 
+			rankings[i]["winRank"] +
+			", \"lossRank\": " + 
+			rankings[i]["lossRank"] +
+			"},", function(err) {if(err) {return console.log(err);} });
+
+		// console.log("{\"fullName\": \"" + rankings[i]["fullName"] + 
+		// 	"\", \"wins\": " + 
+		// 	rankings[i]["win"] + 
+		// 	", \"losses\": " + 
+		// 	rankings[i]["loss"] +
+		// 	", \"winRank\": " + 
+		// 	rankings[i]["winRank"] +
+		// 	", \"lossRank\": " + 
+		// 	rankings[i]["lossRank"] +
+		// 	"},"
+		// );
+	}
+}
---
+++ resources/js/processing/getStandings.js
@@ -0,0 +1,18 @@
+const unirest = require('unirest');
+const fs = require ('fs');
+
+unirest.get("https://api-nba-v1.p.rapidapi.com/standings/standard/2018")
+.header("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com")
+.header("X-RapidAPI-Key", "13bddf6d9fmsh46f9643a34ba3a1p1d3dd1jsn83317f761bc8")
+.end(function (result) {
+	console.log(result.status, result.headers, result.body);
+
+	for (var i = result.body.api.standings.length - 1; i >= 0; i--){
+		fs.appendFile("standings.json", JSON.stringify(result.body.api.standings[i]) + "," , function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+		    console.log("standings " + i + " saved");
+		});	
+	}
+});
