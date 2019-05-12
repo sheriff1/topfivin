751e762b9415fe72a539d0cac61c8752de84f820
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-12 16:53:56 -0700

    Add even logo spacing

--- resources/js/dom/fetchData.js
+++ resources/js/dom/fetchData.js
@@ -5,7 +5,7 @@ $.getJSON("resources/data/rankings.json", function(json) {
     console.log(rankings[0]);
 
     for (var i = rankings.length - 1; i >= 0; i--) {
-        $("tbody").append("<tr><td><img class=\"team-logo\" src= \"" + rankings[i]['logo'] + "\">" +
+        $("tbody").append("<tr><td class=\"logo-data\"><img class=\"team-logo\" src= \"" + rankings[i]['logo'] + "\">" +
             "</td><td>" + rankings[i]['fullName'] +
             "</td><td>" + rankings[i]['wins'] +
             "</td><td>" + rankings[i]['losses'] +
