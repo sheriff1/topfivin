a8bc104f5e98eb7f03b509070386bb7a03a2d1a6
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-16 22:03:59 -0700

    dropdown added to be able to change teams.

--- resources/js/dom/interactive-view.js
+++ resources/js/dom/interactive-view.js
@@ -1,18 +1,23 @@
 var rankings = [];
+var colors = [];
+
+$.getJSON("resources/data/colors.json", function(json) {
+    colors = json;
+});
 
 $.getJSON("resources/data/rankingsV2.json", function(json) {
     rankings = json;
     console.log(rankings[0]);
 
     for (var i = 0; i < rankings.length; i++) {
-        var dropdownTeamItem = "<a class=\"dropdown-item\">" + rankings[i]['fullName'] + "</a>";
-        $(".dropdown-menu").append(dropdownTeamItem);
+        var dropdownTeamItem = "<option class=\"dropdown-item\">" + rankings[i]['fullName'] + "</option>";
+        $(".dropdown").append(dropdownTeamItem);
     }
 
     //NAME AND LOGO FOR HERO
     $(".team-select-team-logo").attr("src", rankings[0]['logo']);
     let nameSplit = rankings[0]['fullName'].split(" ");
-    console.log(nameSplit.length);
+
     if (nameSplit.length == 2) {
         document.getElementsByClassName("city-line")[0].textContent = nameSplit[0];
         document.getElementsByClassName("team-name-line")[0].textContent = nameSplit[1].toUpperCase();
@@ -29,14 +34,62 @@ $.getJSON("resources/data/rankingsV2.json", function(json) {
         rankings[0].data[0].rank +
         "</th></tr>" +
         "<tr><th >" +
-        rankings[1].data[1].stat.toUpperCase() +
+        rankings[0].data[1].stat.toUpperCase() +
         "</th><th class=\'text-right\'> #" +
-        rankings[1].data[1].rank +
+        rankings[0].data[1].rank +
         "</th></tr>" +
         "<tr><th>" +
-        rankings[2].data[2].stat.toUpperCase() +
+        rankings[0].data[2].stat.toUpperCase() +
         "</th><th class=\'text-right\'> #" +
-        rankings[2].data[2].rank +
+        rankings[0].data[2].rank +
         "</th></tr>";
     $(".rank-listing-data").append(rankToAdd);
+});
+
+$(function() {
+    $(".dropdown").change(function() {
+        //console.log($('option:selected', this).text());
+
+        var nextTeam = _.findWhere(rankings, { fullName: $('option:selected', this).text() });
+
+        //CHANGE TEAM NAME & CITY NAME
+        let nameSplit = nextTeam['fullName'].split(" ");
+
+        if (nameSplit.length == 2) {
+            document.getElementsByClassName("city-line")[0].textContent = nameSplit[0];
+            document.getElementsByClassName("team-name-line")[0].textContent = nameSplit[1].toUpperCase();
+        } else if (nameSplit.length == 3) {
+            document.getElementsByClassName("city-line")[0].textContent = nameSplit[0] + " " + nameSplit[1];
+            document.getElementsByClassName("team-name-line")[0].textContent = nameSplit[2].toUpperCase();
+        }
+
+        //CHANGE LOGO
+        $(".team-select-team-logo").attr("src", nextTeam['logo']);
+
+        //CHANGE BACKGROUND COLOR
+        var nextTeamColor = _.findWhere(colors, { fullName: $('option:selected', this).text() });
+        $(".bottom-part").css({ "backgroundColor": nextTeamColor.colors });
+
+        //CHANGE RANK
+        $(".rank-listing-data tr").remove();
+
+        var rankToAdd =
+            "<tr><th>" +
+            nextTeam.data[0].stat.toUpperCase() +
+            "</th><th class=\'text-right\'> #" +
+            nextTeam.data[0].rank +
+            "</th></tr>" +
+            "<tr><th >" +
+            nextTeam.data[1].stat.toUpperCase() +
+            "</th><th class=\'text-right\'> #" +
+            nextTeam.data[1].rank +
+            "</th></tr>" +
+            "<tr><th>" +
+            nextTeam.data[2].stat.toUpperCase() +
+            "</th><th class=\'text-right\'> #" +
+            nextTeam.data[2].rank +
+            "</th></tr>";
+        $(".rank-listing-data").append(rankToAdd);
+
+    });
 });
