8c5ce0ccff2a7b2b48fb42a5203bc4385d94b52e
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-17 22:51:00 -0700

    update to game details scraping method

---
+++ resources/js/processing/getGameDetailsByTeam.js
@@ -0,0 +1,172 @@
+const fs = require('fs');
+const _ = require('underscore');
+
+var gameDetailsRaw = [];
+var gameDetailsByTeam = [];
+
+gameDetailsRaw = JSON.parse(fs.readFileSync('/Library/WebServer/Documents/topfivin/resources/data/gameDetailsRaw.json', 'utf8'));
+console.log(gameDetailsRaw);
+
+for (var i = 0; i < gameDetailsRaw.length; i++) {
+
+    //VISITING TEAM
+    if (_.findWhere(gameDetailsByTeam, { fullName: gameDetailsRaw[i].body.api.game[0].vTeam.fullName }) == null) {
+        //ADD ITEM TO ARRAY FOR VISITING TEAM
+        var toBeAdded = {};
+
+        toBeAdded.fullName = gameDetailsRaw[i].body.api.game[0].vTeam.fullName;
+
+        var gameDurationToMinutes = gameDetailsRaw[i].body.api.game[0].gameDuration.split(":");
+        gameDurationToMinutes = Number(gameDurationToMinutes[0]) * 60 + Number(gameDurationToMinutes[1]);
+
+        toBeAdded.gameDuration = gameDurationToMinutes;
+
+        toBeAdded.timesTied = Number(gameDetailsRaw[i].body.api.game[0].timesTied);
+
+        toBeAdded.leadChanges = Number(gameDetailsRaw[i].body.api.game[0].leadChanges);
+
+        toBeAdded.points = Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.points);
+
+        for (var a = 0; a < gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore.length; a++) {
+            switch (a) {
+                case 0:
+                    toBeAdded.q1Points = Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                case 1:
+                    toBeAdded.q2Points = Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                case 2:
+                    toBeAdded.q3Points = Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                case 3:
+                    toBeAdded.q4Points = Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                default:
+                    toBeAdded.overtimePoints = Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+            }
+        }
+
+        gameDetailsByTeam.push(toBeAdded);
+
+    } else {
+        //UPDATE ITEM IN ARRAY FOR VISITING TEAM
+        var toBeUpdated = _.findWhere(gameDetailsByTeam, { fullName: gameDetailsRaw[i].body.api.game[0].vTeam.fullName });
+
+        var gameDurationToMinutes = gameDetailsRaw[i].body.api.game[0].gameDuration.split(":");
+        gameDurationToMinutes = Number(gameDurationToMinutes[0]) * 60 + Number(gameDurationToMinutes[1]);
+
+        toBeUpdated.gameDuration += gameDurationToMinutes;
+
+        toBeUpdated.timesTied += Number(gameDetailsRaw[i].body.api.game[0].timesTied);
+
+        toBeUpdated.leadChanges += Number(gameDetailsRaw[i].body.api.game[0].leadChanges);
+
+        toBeUpdated.points += Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.points);
+
+        for (var a = 0; a < gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore.length; a++) {
+            switch (a) {
+                case 0:
+                    toBeUpdated.q1Points += Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                case 1:
+                    toBeUpdated.q2Points += Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                case 2:
+                    toBeUpdated.q3Points += Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                case 3:
+                    toBeUpdated.q4Points += Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+                default:
+                    toBeUpdated.overtimePoints += Number(gameDetailsRaw[i].body.api.game[0].vTeam.score.linescore[a]);
+                    break;
+            }
+        }
+
+    }
+
+    //HOME TEAM
+    if (_.findWhere(gameDetailsByTeam, { fullName: gameDetailsRaw[i].body.api.game[0].hTeam.fullName }) == null) {
+        //ADD ITEM TO ARRAY FOR HOME TEAM
+        var toBeAdded = {};
+
+        toBeAdded.fullName = gameDetailsRaw[i].body.api.game[0].hTeam.fullName;
+
+        var gameDurationToMinutes = gameDetailsRaw[i].body.api.game[0].gameDuration.split(":");
+        gameDurationToMinutes = Number(gameDurationToMinutes[0]) * 60 + Number(gameDurationToMinutes[1]);
+
+        toBeAdded.gameDuration = gameDurationToMinutes;
+
+        toBeAdded.timesTied = Number(gameDetailsRaw[i].body.api.game[0].timesTied);
+
+        toBeAdded.leadChanges = Number(gameDetailsRaw[i].body.api.game[0].leadChanges);
+
+        toBeAdded.points = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.points);
+
+        for (var a = 0; a < gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore.length; a++) {
+            switch (a) {
+                case 0:
+                    toBeAdded.q1Points = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                case 1:
+                    toBeAdded.q2Points = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                case 2:
+                    toBeAdded.q3Points = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                case 3:
+                    toBeAdded.q4Points = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                default:
+                    toBeAdded.overtimePoints = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+            }
+        }
+
+        gameDetailsByTeam.push(toBeAdded);
+
+    } else {
+        //UPDATE ITEM IN ARRAY FOR HOME TEAM
+        var toBeUpdated = _.findWhere(gameDetailsByTeam, { fullName: gameDetailsRaw[i].body.api.game[0].hTeam.fullName });
+
+        var gameDurationToMinutes = gameDetailsRaw[i].body.api.game[0].gameDuration.split(":");
+        gameDurationToMinutes = Number(gameDurationToMinutes[0]) * 60 + Number(gameDurationToMinutes[1]);
+
+        toBeUpdated.gameDuration += gameDurationToMinutes;
+
+        toBeUpdated.timesTied += Number(gameDetailsRaw[i].body.api.game[0].timesTied);
+
+        toBeUpdated.leadChanges += Number(gameDetailsRaw[i].body.api.game[0].leadChanges);
+
+        toBeUpdated.points += Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.points);
+
+        for (var a = 0; a < gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore.length; a++) {
+            switch (a) {
+                case 0:
+                    toBeUpdated.q1Points += Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                case 1:
+                    toBeUpdated.q2Points += Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                case 2:
+                    toBeUpdated.q3Points += Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                case 3:
+                    toBeUpdated.q4Points += Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+                default:
+                    toBeUpdated.overtimePoints = Number(gameDetailsRaw[i].body.api.game[0].hTeam.score.linescore[a]);
+                    break;
+            }
+        }
+
+    }
+}
+
+fs.appendFile("/Library/WebServer/Documents/topfivin/resources/data/gameDetailsByTeam.json", JSON.stringify(gameDetailsByTeam), function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("gameDetailsByTeam.json saved");
+});
---
+++ resources/js/processing/getGameDetailsRaw.js
@@ -0,0 +1,24 @@
+const unirest = require('unirest');
+const fs = require('fs');
+const _ = require('underscore');
+
+//going to get gameDetails from games 4387 to 6075
+
+var startOfRegSeason = 4387;
+var endOfRegSeason = 6075; //gets all NBA reg. season games
+
+var gameDetailsByTeam = [];
+
+for (var i = startOfRegSeason; i < 4390; i++) {
+    unirest.get("https://api-nba-v1.p.rapidapi.com/gameDetails/" + i)
+        .header("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com")
+        .header("X-RapidAPI-Key", "13bddf6d9fmsh46f9643a34ba3a1p1d3dd1jsn83317f761bc8")
+        .end(function(result) {
+
+            fs.appendFile("/Library/WebServer/Documents/topfivin/resources/data/gameDetailsRaw.json", JSON.stringify(result) + ",", function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        });
+}
