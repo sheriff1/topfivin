89059a3cf446509a48eeee5799239846295bf9d1
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-18 07:44:34 -0700

    file cleanup

--- resources/js/processing/createRankings.js
+++ resources/js/processing/createRankings.js
@@ -3,27 +3,114 @@ const _ = require('underscore');
 
 let rankings = [];
 
-let teams = JSON.parse(fs.readFileSync('resources/data/teams.json', 'utf8'));
+let teams = JSON.parse(fs.readFileSync('../../data/teams.json', 'utf8'));
 // console.log(teams);
 
-let standings = JSON.parse(fs.readFileSync('resources/data/standings.json', 'utf8'));
+let standings = JSON.parse(fs.readFileSync('../../data/standings.json', 'utf8'));
 // console.log(standings);
-buildSimple(teams, standings);
+
+let gameDetails = JSON.parse(fs.readFileSync('../../data/gameDetailsByTeam-sample.json', 'utf8'));
+
+buildStandings(teams, standings);
+//buildGameDetails(rankings, gameDetails);
 sortSimple(rankings);
 
-function buildSimple(teams, standings) {
 
-    console.log("buildSimple called");
+//* UNCOMMENT OUT BELOW WHEN GAME DETAILS ARE READY !!!
+
+// function buildGameDetails(rankings, gameDetails) {
+//     console.log("buildGameDetails called");
+
+//     for (var i = 0; i < gameDetails.length; i++) {
+//         //Find team by name
+//         var team = _.findWhere(rankings, { fullName: gameDetails[i].fullName });
+//         //Add data[x] for each item 
+//         team.data[11] = {
+//             rank: 4,
+//             value: gameDetails[i].gameDuration,
+//             stat: "Game Duration"
+//         }
+//     }
+// }
+
+
+function buildStandings(teams, standings) {
+
+    console.log("buildStandings called");
 
     //Builds 'ranking' array to be manipulated
     for (var i = 0; i <= teams.length - 1; i++) {
         var toBeAdded = {};
+        toBeAdded.data = [];
+        toBeAdded.data[0] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).win),
+            stat: "Wins"
+        }
+
+        toBeAdded.data[1] = {
+            rank: "",
+            value: value = Number(_.findWhere(standings, { teamId: teams[i].teamId }).loss),
+            stat: "Losses"
+        }
+
+        toBeAdded.data[2] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).streak),
+            stat: "Streak"
+        }
+
+        toBeAdded.data[3] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).division.win),
+            stat: "Division Wins"
+        }
+
+        toBeAdded.data[4] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).division.loss),
+            stat: "Division Losses"
+        }
+
+        toBeAdded.data[5] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).conference.win),
+            stat: "Conference Wins"
+        }
+
+        toBeAdded.data[6] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).conference.loss),
+            stat: "Conference Losses"
+        }
+
+        toBeAdded.data[7] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).home.win),
+            stat: "Home Wins"
+        }
+
+        toBeAdded.data[8] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).home.loss),
+            stat: "Home Losses"
+        }
+
+        toBeAdded.data[9] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).away.win),
+            stat: "Away Wins"
+        }
+
+        toBeAdded.data[10] = {
+            rank: "",
+            value: Number(_.findWhere(standings, { teamId: teams[i].teamId }).away.loss),
+            stat: "Away Losses"
+        }
+
         toBeAdded.fullName = teams[i].fullName;
-        toBeAdded.wins = Number(_.findWhere(standings, { teamId: teams[i].teamId }).win);
-        toBeAdded.losses = Number(_.findWhere(standings, { teamId: teams[i].teamId }).loss);
-        toBeAdded.streak = Number(_.findWhere(standings, { teamId: teams[i].teamId }).streak);
-        toBeAdded.divisionWins = Number(_.findWhere(standings, { teamId: teams[i].teamId }).division.win);
         toBeAdded.logo = teams[i].logo;
+
         rankings.push(toBeAdded);
     }
 }
@@ -32,68 +119,17 @@ function sortSimple(rankings) {
 
     console.log("sortSimple called");
 
-    //Add Win Rank
-    rankings = _.sortBy(rankings, 'wins');
-    for (var i = 0; i <= rankings.length - 1; i++) {
-        rankings[i]["winsRank"] = rankings.length - i;
+    // console.log(rankings[0].data.length);
+
+    for (var r = 0; r < rankings[0].data.length; r++) {
+        rankings = rankings.sort(function(a, b) {
+            return a.data[r].value - b.data[r].value;
+        });
+        for (var i = 0; i < rankings.length; ++i) {
+            rankings[i].data[r].rank = 30 - i;
+        }
     }
 
-    // //Add Loss Rank
-    rankings = _.sortBy(rankings, 'losses');
-    for (var i = 0; i <= rankings.length - 1; i++) {
-        rankings[i]["lossesRank"] = rankings.length - i;
-    }
+    fs.appendFile("../../data/rankingsV2.json", JSON.stringify(rankings), function(err) { if (err) { return console.log(err); } });
 
-    //Add Streak Rank
-    rankings = _.sortBy(rankings, 'streak');
-    for (var i = 0; i <= rankings.length - 1; i++) {
-        rankings[i]["streakRank"] = rankings.length - i;
-    }
-
-    //Add Divisional Wins
-    rankings = _.sortBy(rankings, 'divisionWins');
-    for (var i = 0; i <= rankings.length - 1; i++) {
-        rankings[i]["divisionWinsRank"] = rankings.length - i;
-    }
-
-
-    fs.appendFile("resources/data/rankings.json", "[", function(err) { if (err) { return console.log(err); } });
-
-    //Print to see results
-    for (var i = rankings.length - 1; i >= 0; i--) {
-
-        fs.appendFile("resources/data/rankings.json",
-            "{\"fullName\": \"" +
-            rankings[i]["fullName"] +
-
-            "\", \"logo\": \"" +
-            rankings[i]["logo"] +
-
-            //Queue Wins
-            "\", \"wins\": " +
-            rankings[i]["wins"] +
-            ", \"winsRank\": " +
-            rankings[i]["winsRank"] +
-
-            //Queue Losses
-            ", \"losses\": " +
-            rankings[i]["losses"] +
-            ", \"lossesRank\": " +
-            rankings[i]["lossesRank"] +
-
-            //Queue Recent Streak
-            ", \"streak\": " +
-            rankings[i]["streak"] +
-            ", \"streakRank\": " +
-            rankings[i]["streakRank"] +
-
-            //Queue Division Wins Streak
-            ", \"divisionWins\": " +
-            rankings[i]["divisionWins"] +
-            ", \"divisionWinsRank\": " +
-            rankings[i]["divisionWinsRank"] +
-
-            "},",
-            function(err) { if (err) { return console.log(err); } });
-    }
 }
