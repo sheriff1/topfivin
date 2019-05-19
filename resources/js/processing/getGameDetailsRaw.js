ddb48a3b8afb60a0a399f952d5eea36baa5c65f4
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-19 09:46:20 -0700

    update to some processing files

--- resources/js/processing/getGameDetailsRaw.js
+++ resources/js/processing/getGameDetailsRaw.js
@@ -2,20 +2,16 @@ const unirest = require('unirest');
 const fs = require('fs');
 const _ = require('underscore');
 
-//going to get gameDetails from games 4387 to 6075
+var gameDetails = [];
+var gameIds = JSON.parse(fs.readFileSync('_gameIds.json', 'utf8'));
 
-var startOfRegSeason = 4387;
-var endOfRegSeason = 6075; //gets all NBA reg. season games
-
-var gameDetailsByTeam = [];
-
-for (var i = startOfRegSeason; i < 4388; i++) { //4390 to be changed to endOfRegSeason when I feel like spending the money
-    unirest.get("https://api-nba-v1.p.rapidapi.com/gameDetails/" + i)
+for (var i = 0; i < 1; i++) {
+    unirest.get("https://api-nba-v1.p.rapidapi.com/gameDetails/" + gameIds[i])
         .header("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com")
         .header("X-RapidAPI-Key", "[ADD API KEY HERE]")
         .end(function(result) {
 
-            fs.appendFile("gameDetailsRaw.json", JSON.stringify(result) + ",", function(err) {
+            fs.appendFile("_gameDetailsRaw.json", JSON.stringify(result) + ",", function(err) {
                 if (err) {
                     return console.log(err);
                 }
