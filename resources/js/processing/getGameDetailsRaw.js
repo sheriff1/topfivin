91ef050bfada5b0cbcaefeb24a5d543f1874b65f
Sheriff <sheriff.jolaoso@gmail.com>
2019-05-19 11:37:21 -0700

    filename update

--- resources/js/processing/getGameDetailsRaw.js
+++ resources/js/processing/getGameDetailsRaw.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const _ = require('underscore');
 
 var gameDetails = [];
-var gameIds = JSON.parse(fs.readFileSync('_gameIds.json', 'utf8'));
+var gameIds = JSON.parse(fs.readFileSync('__gameIds.json', 'utf8'));
 
 for (var i = 0; i < 1; i++) {
     unirest.get("https://api-nba-v1.p.rapidapi.com/gameDetails/" + gameIds[i])
